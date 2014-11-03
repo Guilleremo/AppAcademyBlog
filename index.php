78ad924692f325ef7acf9e9d864d63a67b0b61e3
Guillermo Andrew Hermosillo <g.andrewhermosillo@gmail.com>
2014-11-03 13:17:08 -0800

    video 16 commit

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Guillermo's php project!</title>
-</head>
-<body>
-	<?php
-		require_once(__DIR__ . "/controller/create-db.php");
-		//direct connection to the database 
-	?>
-</body>
-</html>
+<?php
+	require_once(__DIR__ . "/view/header.php");
+	require_once(__DIR__ . "/controller/create-db.php");
+	require_once(__DIR__ . "/view/footer.php");
+	//direct connection to the database 
+?>
---
+++ view/header.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Guillermo's php project!</title>
+</head>
+<body>
