f906513e7c9dfd149ca06ff7c61ae974feb0723a
Guillermo Andrew Hermosillo <g.andrewhermosillo@gmail.com>
2014-11-04 12:28:15 -0800

    i finished the $path variable and i have navigation, post, and config and put comments and put the code i needed

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 	require_once(__DIR__ . "/view/header.php");
+	//this require once has the connection from here to the folder view to header.php
+	require_once(__DIR__ . "/view/navigation.php");
+	//this require once has the connection from here to the folder view to navigation.php
 	require_once(__DIR__ . "/controller/create-db.php");
-	require_once(__DIR__ . "/view/footer.php");
 	//direct connection to the database 
+	require_once(__DIR__ . "/view/footer.php");
+	//this require once has the connection from here to the folder view to footer.php
 ?>
