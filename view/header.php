266ce5aef5ec5e2380777ca26d33cc308f27ff66
Guillermo Andrew Hermosillo <g.andrewhermosillo@gmail.com>
2014-11-06 12:12:00 -0800

    i did commits and the media query

--- view/header.php
+++ view/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+<!--This viewport allows us to use our media query in our css.-->
+	<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 	<title>Guillermo's php project!</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
