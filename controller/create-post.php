c49bf8ad47bbc8720de849868bcc771d46c5918f
Guillermo Andrew Hermosillo <g.andrewhermosillo@gmail.com>
2014-11-12 12:26:33 -0800

    comments and blog post finished

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,7 @@
 <?php
-	$title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	//This post means it is receiving data called title and filtering it
-	$post = filer_inpt(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 	//this post means it is receiving data called post and is filtering it
 
 	echo "<p>Title: $title</p>";
