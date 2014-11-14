261e807ae80898b7534ef16978c936cd07434abe
Guillermo Andrew Hermosillo <g.andrewhermosillo@gmail.com>
2014-11-14 12:21:58 -0800

    i made a new database and made a new class

--- model/config.php
+++ model/config.php
@@ -1,2 +1,8 @@
 <?php 
 	$path = "/phpproject/";
+	
+	$host = "localhost";
+	$username = "root";
+	$password = "root";
+	$database = "blog_db";
+	//stores all of the variables
--- model/database.php
+++ model/database.php
@@ -1,6 +1,11 @@
 <?php
-	$host = "localhost";
-	$username = "root";
-	$password = "root";
-	$database = "blog_db";
-	//stores all of the variables
+	class Database {
+	private $connection;
+	private $host;
+	private $username;
+	private $password;
+	private $database;
+}
+//weve created global variables that can be accessed through out our databse class.
+//weve set the visibilty and they can only be accessed from the class and we wont be able to access them from anywhere else.
+//That way when we create the new object, these variable would be hidden and no one would be able to modify them.
